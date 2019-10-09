e4b0eed1ba984f3f7b6f47f21dda0d19cc527c2c
John Miner <john.douglas.miner.iv@gmail.com>
2019-10-08 22:26:47 -0700

    Removing requests

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 from bson.objectid import ObjectId
-import requests
 import os
 from dotenv import load_dotenv
 load_dotenv()
